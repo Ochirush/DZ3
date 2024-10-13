366dc70147ecabfcbffc924eea7ba26d00dba612
Ochirush <ulemdhievka@mail.ru>
2024-10-13 17:33:00 +0300

    first commit

---
+++ src/main/java/ru/mirea/ulemdzhievob/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.ulemdzhievob;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/ru/mirea/ulemdzhievob/PokemonStage.java
@@ -0,0 +1,4 @@
+package ru.mirea.ulemdzhievob;
+
+public enum PokemonStage {
+}
